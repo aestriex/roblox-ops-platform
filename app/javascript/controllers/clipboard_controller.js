b6cd8e79a86fd48ffb457e2b9ca705cd9a337021
aestriex <caidenm@duck.com>
2026-08-10 21:23:11 -0700

    [upd] Posting page UI QoL

--- app/javascript/controllers/clipboard_controller.js
+++ app/javascript/controllers/clipboard_controller.js
@@ -1,9 +1,17 @@
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
-  static targets = ["source"]
+  static targets = ["source", "copyIcon", "checkIcon"]
 
   copy() {
     navigator.clipboard.writeText(this.sourceTarget.value)
+
+    this.copyIconTarget.classList.add("hidden")
+    this.checkIconTarget.classList.remove("hidden")
+
+    setTimeout(() => {
+      this.copyIconTarget.classList.remove("hidden")
+      this.checkIconTarget.classList.add("hidden")
+    }, 2000)
   }
 }
